bcaf3850b36ceddb4957ee1214449d7f89f9d853
DilyaraL <latipova8d@gmail.com>
2024-07-06 23:26:46 +0300

    urls: сat_products

--- clothing_store/store/urls.py
+++ clothing_store/store/urls.py
@@ -5,7 +5,7 @@ from .views import *
 urlpatterns = [
     path('', Home.as_view(), name='home'),
     path('products/', Product.as_view(), name='products'),
-    path('products/<slug:cat_slug>/', Product.as_view(), name='сat_products'),
+    path('products/categories/<slug:cat_slug>/', Product.as_view(), name='сat_products'),
     path('products/<slug:prod_slug>', ShowProduct.as_view(), name='one_product'),
     path('orders/<int:pk>', ShowOrder.as_view()),
     path('orders/', Orders.as_view(), name='orders'),
